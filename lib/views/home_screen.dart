0a5fbc2e00a18baf866fb1bcd558116a035c83a9
Rajaa <rajaa.ter.98@gmail.com>
2022-04-12 18:16:22 +0300

    home screen

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
+
+import '../utils/theme/app_colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -7,24 +10,69 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: const Drawer(),
+      backgroundColor: Colors.white,
+
       appBar: AppBar(
-        leading: const Icon(Icons.location_on),
-        title: Text('damascus'.tr()),
+
+        title: Text('damascus'.tr(), style:Theme.of(context).textTheme.headline3),
         actions: [
-          IconButton(onPressed: () {
-
-          }, icon: const Icon(Icons.filter),),
-          IconButton(onPressed: () {
-
-          }, icon: const Icon(Icons.search),)
+          IconButton(onPressed: () {},
+            icon: const Icon(Icons.compare_arrows),),
+          IconButton(onPressed: () {},
+            icon: const Icon(Icons.search),)
         ],
       ),
-      floatingActionButton: FloatingActionButton(onPressed: () {  },),
-      drawer: const Drawer(),
-      bottomNavigationBar: BottomNavigationBar(items:  [
-        BottomNavigationBarItem(icon:Icon(Icons.home), label: 'home'.tr()),
-        BottomNavigationBarItem(icon:Icon(Icons.home),  label: 'my reals'.tr()),
-      ],),
+
+      body: ListView.separated(
+        padding: EdgeInsets.symmetric(vertical: 20.0),
+        separatorBuilder: (_, int index) => SizedBox(),
+        itemCount: 20,
+        itemBuilder: (context, index) {
+          return Row(
+              mainAxisAlignment: index % 2 == 0
+                  ? MainAxisAlignment.end
+                  : MainAxisAlignment.start,
+              children: [ Container(
+                width: 300,
+                height: 200,
+                alignment: AlignmentDirectional.bottomCenter,
+                decoration: BoxDecoration(
+                    color: AppColors.secondary,
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/reals')),
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20))
+                ),
+                child: Container(
+                  height: 50,
+                  color: AppColors.primary,
+                  child: DefaultTextStyle.merge(
+                    style: TextStyle(color: AppColors.secondary),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text('Price:1200'),
+                        Text("Type:home"),
+                        Icon(Icons.favorite),
+
+                      ],
+
+                    ),
+                  ),
+
+                ),
+              ),
+              ]
+          );
+        },
+      ),
+
+
+      //bottomNavigationBar: BottomNavigationBar(items: [BottomNavigationBarItem(icon: Icon(Icons.person))BottomNavigationBarItem(icon: Icon(Icons.home)),BottomNavigationBarItem(icon: Icon(Icons.home_work_outlined)),],),
     );
   }
 }
+
+
